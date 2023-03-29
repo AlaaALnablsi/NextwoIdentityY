74ed2547bf758ab3d6468809ebb2e945ac085254
Dell <Dell@DESKTOP-1UIH755>
2023-03-30 01:40:24 +0300

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NextwoIdentity.Data;
+using NextwoIdentity.Models;
+
+namespace NextwoIdentity.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private NextwoDbContext db;
+        private RoleManager<IdentityRole> roleManager;
+
+
+        public CategoriesController(NextwoDbContext _db, RoleManager<IdentityRole> _roleManager)
+        {
+            this.db = _db;
+            this.roleManager = _roleManager;
+        }
+        public IActionResult Index()
+        {
+
+            return View(db.Categorys);
+        }
+
+        public IActionResult CreateCategory()
+        {
+            ViewBag.allCateg = new SelectList(db.Categorys, "CategoryId", "CategoryName");
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var test = db.Categorys.FirstOrDefault(c => c.CategoryName! == category.CategoryName!);
+                if (test == null)
+                {
+                    db.Categorys.Add(category);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.check = "Exists";
+                }
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var data = db.Categorys.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categorys.Update(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var data = db.Categorys.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            var data = db.Categorys.Find(category.CategoryId);
+            if (data == null)
+            {
+                return RedirectToAction("Index");
+            }
+            db.Categorys.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var data = db.Categorys.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        //public IActionResult test(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var test = db.Categorys.FirstOrDefault(c => c.CategoryName! == category.CategoryName!);
+        //        if (test == null)
+        //        {
+        //            db.Categorys.Add(category);
+        //            db.SaveChanges();
+        //            return View(test);
+        //        }
+        //        else
+        //        {
+        //            ViewBag.check = "Exists";
+        //        }
+        //    }
+        //    return View(category);
+        //}
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NextwoIdentity.Data;
+using NextwoIdentity.Models;
+
+namespace NextwoIdentity.Controllers
+{
+    [Authorize]
+
+    public class ProductController : Controller
+    {
+
+     
+        private NextwoDbContext db;
+        private RoleManager<IdentityRole> roleManager;
+
+
+        public ProductController( NextwoDbContext _db, RoleManager<IdentityRole> _roleManager)
+        {
+            this.db = _db;
+            this.roleManager = _roleManager;
+        }
+        public IActionResult AllProduct()
+        {
+            return View(db.Products.Include(x => x.Category));
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateProduct() {
+            ViewBag.allCateg = new SelectList(db.Categorys, "CategoryId", "CategoryName");
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("AllProduct");
+            }
+            return View(product);
+        }
+
+
+      
+        public ActionResult ALLCategory() {
+
+                return View(db.Categorys);
+
+        }
+        [Authorize(Roles = "Admin")]
+
+        public ActionResult CreateCategory() {
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task <ActionResult> CreateCategory(Category category)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                db.Categorys.Add(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction("ALLCategory");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            var data = db.Products.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Update(product);
+                db.SaveChanges();
+                return RedirectToAction("AllProduct");
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            var data = db.Products.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            var data = db.Products.Find(product.ProductId);
+            if (data == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            db.Products.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("AllProduct");
+        }
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            var data = db.Products.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("AllProduct");
+            }
+            return View(data);
+        }
+    }
+}
